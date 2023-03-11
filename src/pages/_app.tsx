a6221776137e55682dc16d989f7bd965b6d3b932
Marco Portocarrero <marcoa.portocarrero@gmail.com>
2023-03-10 22:29:15 -0500

    starting again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
