f8cc3a32cc797def065b8bb93d5a68c21ca8fd75
Marco Portocarrero <marcoa.portocarrero@gmail.com>
2023-03-10 23:22:51 -0500

    added tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import "tailwindcss/tailwind.css"
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+import { trpc } from '../utils/trpc';
+
+export default trpc.withTRPC(MyApp);
